b18c92bcb3cde6bd25c24ef755e1bdbca85f75fc
Ravi Ranjan Singh <raghavs821@gmail.com>
2023-10-14 01:26:32 +0530

    The kitsune IDS(tensor flow implementation) is added to the tardigrade code

--- tardigrade/ids/__init__.py
+++ tardigrade/ids/__init__.py
@@ -1,9 +1,8 @@
-from .kitsune import KitModel, KeyedKitModel
-from .awesome_ids import AwesomeIDS
+# from .kitsune import KitModel, KeyedKitModel
+from .awesome_ids import awesome_ids_model
+# do we have to make any changes here to create the function inside awesome ids , I dont want to call kitsune and keyed kitsune here
+
 
 __all__ = [
-    "AwesomeIDS",
-    "KitModel",
-    "KeyedKitModel",
-    "evaluation_metrics"
+    "AwesomeIDS"
     ]
